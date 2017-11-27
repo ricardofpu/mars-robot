1267da456cbb53bc015eabfc5bfd59baff81c8be
Ricardo Marques <ricardo.borges@zup.com.br>
2017-11-27 10:56:21 -0200

    commands to test

--- mars-robot-web/src/main/java/br/com/mars/robot/web/service/RobotService.java
+++ mars-robot-web/src/main/java/br/com/mars/robot/web/service/RobotService.java
@@ -35,6 +35,6 @@ public class RobotService {
     }
 
     private boolean isMoveCommand(char command) {
-        return command == Commands.MOVE.valueOf();
+        return Commands.MOVE.valueOf() == command;
     }
 }
